dacc4062660f5eacbf6685c02d6049af8d4942be
rich <rich@shopittome.com>
2012-10-04 21:16:21 -0700

    util_windows: make build for windows

--- util_windows.go
+++ util_windows.go
@@ -4,15 +4,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// +build !plan9,!windows
+// +build !plan9
 
 package terminal
 
+
 import (
-	"fmt"
+//	"fmt"
+	"path/filepath"
 	"os"
-	"os/signal"
-	"strconv"
+//	"os/signal"
+//	"strconv"
 	"syscall"
 )
 
